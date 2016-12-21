adc5445cae262b37e5fdb5ef4e6ef54aede1d543
Nicholas <Nicholas@DESKTOP-ONK2K1O>
2016-12-21 15:29:41 +0800

    添加项目文件。

---
+++ myfirstCSharp/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace myfirstCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.p1 = new System.Windows.Forms.Label();
+            this.p2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.remaining = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnReroll = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.info = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(668, 250);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(143, 62);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start/Stop";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.Transparent;
+            this.p1.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.p1.Location = new System.Drawing.Point(109, 131);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(329, 84);
+            this.p1.TabIndex = 1;
+            this.p1.Text = "候选人1";
+            this.p1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // p2
+            // 
+            this.p2.AutoSize = true;
+            this.p2.BackColor = System.Drawing.Color.Transparent;
+            this.p2.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.p2.Location = new System.Drawing.Point(578, 131);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(329, 84);
+            this.p2.TabIndex = 2;
+            this.p2.Text = "候选人2";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // remaining
+            // 
+            this.remaining.AutoSize = true;
+            this.remaining.BackColor = System.Drawing.Color.Transparent;
+            this.remaining.Font = new System.Drawing.Font("宋体", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.remaining.Location = new System.Drawing.Point(29, 26);
+            this.remaining.Name = "remaining";
+            this.remaining.Size = new System.Drawing.Size(283, 52);
+            this.remaining.TabIndex = 3;
+            this.remaining.Text = "剩余人数：";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(461, 120);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(168, 166);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.info);
+            this.panel1.Controls.Add(this.btnReroll);
+            this.panel1.Controls.Add(this.remaining);
+            this.panel1.Controls.Add(this.p1);
+            this.panel1.Controls.Add(this.btnStart);
+            this.panel1.Controls.Add(this.p2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(167, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1055, 392);
+            this.panel1.TabIndex = 6;
+            // 
+            // btnReroll
+            // 
+            this.btnReroll.Location = new System.Drawing.Point(169, 242);
+            this.btnReroll.Name = "btnReroll";
+            this.btnReroll.Size = new System.Drawing.Size(143, 62);
+            this.btnReroll.TabIndex = 5;
+            this.btnReroll.Text = "再给他一次机会";
+            this.btnReroll.UseVisualStyleBackColor = true;
+            this.btnReroll.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // info
+            // 
+            this.info.BackColor = System.Drawing.Color.Transparent;
+            this.info.Font = new System.Drawing.Font("宋体", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.info.Location = new System.Drawing.Point(3, 331);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(1017, 56);
+            this.info.TabIndex = 6;
+            this.info.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1322, 458);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "2016金融专硕相亲会";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label p1;
+        private System.Windows.Forms.Label p2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label remaining;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnReroll;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label info;
+    }
+}
+
---
+++ myfirstCSharp/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+namespace myfirstCSharp
+{
+    public partial class Form1 : Form
+    {
+        List<int> boy = new List<int>();
+        List<int> girl = new List<int>();
+        Dictionary<int, string> vip = new Dictionary<int, string>();
+        System.Media.SoundPlayer sp;
+        public Form1()
+        {
+            InitializeComponent();
+            boy.AddRange(new int[] { 1, 8, 10, 12, 13, 14, 16, 18, 19, 20, 25, 39, 45, 46, 47, 48, 49, 51, 54, 55, 56, 57, 58, 61, 62, 63, 64, 65, 68, 70, 75, 78, 80 });
+            girl.AddRange(new int[] { 2, 3, 4, 5, 6, 7, 9, 11, 15, 17, 21, 22, 23, 24, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 40, 41, 42, 43, 44, 50, 52, 53, 59, 60, 66, 67, 69, 71, 72, 73, 74, 76, 77, 79, 81, 82 });
+            vip.Add(13, "不说了，为涛哥奉上特殊BGM");
+            vip.Add(8, "小秦同学今天盘亏手机一部，请大家关照一下");
+            vip.Add(10,"感谢下伦带逛，带同学们一直high");
+            vip.Add(12, "本码农也出来露个面");
+
+        }
+        private void delete()
+        {
+            if (boy.Count > 0)
+            {
+                boy.Remove(int.Parse(p1.Text));
+                girl.Remove(int.Parse(p2.Text));
+            }
+            else
+            {
+                girl.Remove(int.Parse(p1.Text));
+                girl.Remove(int.Parse(p2.Text));
+            }
+            remaining.Text = "剩余人数：" + (boy.Count + girl.Count);
+        }
+
+        private void changeBgm(UnmanagedMemoryStream bgm)
+        {
+            sp.Stop();
+            sp.Stream = bgm;
+            sp.PlayLooping();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (girl.Count <= 0)
+            {
+                MessageBox.Show("匹配全部完成,大会结束");
+            }
+            else
+            {
+                if (timer2.Enabled)
+                {
+                    btnReroll.PerformClick();
+                }else
+                {
+                    if (timer1.Enabled)
+                    {
+                        timer1.Stop();
+                        //MessageBox.Show( boy.Count + "  " + girl.Count);
+                        if (vip.ContainsKey(int.Parse(p1.Text)))
+                        {
+                            changeBgm(Properties.Resources.bgm);
+                            info.Text = vip[int.Parse(p1.Text)];
+                            //btnReroll.Visible = true;
+                        }
+                        else
+                            changeBgm(Properties.Resources.bgmWaiting);
+                        delete();
+                    }
+                    else
+                    {
+                        info.Text = "";
+                        changeBgm(Properties.Resources.bgmRolling);
+                        btnReroll.Visible = false;
+                        timer1.Start();
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int index1, index2;
+            if (boy.Count > 0)
+            {
+                index1 = rnd.Next(boy.Count);
+                index2 = rnd.Next(girl.Count);
+
+                p1.Text = (boy[index1]).ToString();
+                p2.Text = (girl[index2]).ToString();
+            }
+            else
+            {
+                index1 = rnd.Next(girl.Count);
+                index2 = rnd.Next(girl.Count);
+
+                p1.Text = (girl[index1]).ToString();
+                p2.Text = (girl[girl.Count - 1 - index1]).ToString();
+            }       
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sp = new System.Media.SoundPlayer(global::myfirstCSharp.Properties.Resources.bgmWaiting);
+            sp.PlayLooping();
+        }
+
+        private void btnReroll_Click(object sender, EventArgs e)
+        {
+            if (timer2.Enabled)
+            {
+                timer2.Stop();
+                delete();
+            }
+            else
+            {
+                girl.Add(int.Parse(p2.Text));
+                timer2.Start();
+            }
+                
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(girl.Count);
+            p2.Text = girl[index].ToString();
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            panel1.Left = (ActiveForm.Width - panel1.Width) / 2;
+            panel1.Top = (ActiveForm.Height - panel1.Height) / 2;
+            panel1.Refresh();
+        }
+    }
+}
